d0336a5b7ece95d6428593a143bc545170614419
Victor <victorprg0111@gmail.com>
2024-11-08 03:05:45 +0000

    initial ui and bug fixes

--- script2.js
+++ script2.js
@@ -1,6 +1,26 @@
 let toOperate = ''
 let operator = ''
 let display = ''
+document.querySelector('.clear').addEventListener('click', () => {
+  toOperate = ''
+  operator = ''
+  display = ''
+  update()
+})
+document.querySelector('.erase').addEventListener('click', () => {
+  display = display.substring(0, display.length - 1)
+  update()
+})
+
+document.querySelector('.dot').addEventListener('click', () => {
+  console.log(display)
+  if (display.match(/[0-9]+\./)) {
+    return;
+  }
+
+  display += '.'
+  update()
+})
 
 function update() {
   document.querySelector('.display').innerHTML = display
@@ -27,12 +47,24 @@ function divide (number1, number2) {
 }
 
 function addNumber(number) {
-  display += number
+  display += `${number}`
   document.querySelector('.display').innerHTML = display
 }
 
 function getOperator(signal) {
+  if (display === '') {
+    operator = signal
+    update()
+    return
+  }
   toOperate = operate((parseFloat(toOperate || 0)), (parseFloat(display || 0)), operator)
+  if (isNaN(toOperate)) {
+    toOperate = ''
+    operator = ''
+    display = ''
+    update()
+    return
+  }
   operator = signal
   display = ''
   update()
@@ -40,6 +72,20 @@ function getOperator(signal) {
 }
 
 function operate(number1, number2, operator) {
+  console.log('operating')
+  console.log(number1)
+  console.log(number2)
+
+  if (number1 === '' && number2 === '') {
+    console.log('hello')    
+    return display = ''
+  }
+
+  // if (number1 === 0 || number2 === 0) {
+  //   console.log('hello')    
+  //   return display = ''
+  // }
+
   if (!operator) {
     console.log('hello')
     return display
@@ -64,7 +110,14 @@ function operate(number1, number2, operator) {
 
 function getResult() {
   const result = operate(toOperate, display, operator)
-  display = result
+  if (isNaN(result)) {
+    toOperate = ''
+    operator = ''
+    display = ''
+    update()
+    return
+  }
+  display = `${result}`
   toOperate = ''
   operator = ''
   update()
